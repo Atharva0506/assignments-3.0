b80231fc47784534c901f23465cc0e380e282060
Atharva0506 <atharvan.coder@gmail.com>
2025-01-09 16:32:29 +0530

    promise chain

--- week-2/week-2-async-js/hard (promises)/4-promise-chain.js
+++ week-2/week-2-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,39 @@
  */
 
 function wait1(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t * 1000);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t * 1000);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t * 1000);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
-
+  const start = new Date();
+  return call(t1, t2, t3).then(() => {
+    const end = new Date();
+    return end.getTime() - start.getTime();
+  });
+}
+function call(t1, t2, t3) {
+  return wait1(t1)
+    .then(() => {
+      return wait2(t2);
+    })
+    .then(() => {
+      return wait3(t3);
+    });
 }
 
+console.log(calculateTime(2, 4, 5));
 module.exports = calculateTime;
